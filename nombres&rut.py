2b4e04c09b29b98db0857bed4b38fee1bb22aeeb
Benjamín Miranda <BenjaMiranda19@users.noreply.github.com>
2023-04-14 12:33:05 -0400

    Mi primer commit

---
+++ nombres&rut.py
@@ -0,0 +1,23 @@
+a=(input("Ingrese su nombre:"))
+a_1=(input("Ingrese su RUT:"))
+
+b=(input("Ingrese su nombre:"))
+b_1=(input("Ingrese su RUT:"))
+
+c=(input("Ingrese su nombre:"))
+c_1=(input("Ingrese su RUT:"))
+
+d=(input("Ingrese su nombre:"))
+d_1=(input("Ingrese su RUT:"))
+
+e=(input("Ingrese su nombre:"))
+e_1=(input("Ingrese su RUT:"))
+
+lista_nombre=[a,b,c,d,e]
+lista_RUT=[a_1,b_1,c_1,d_1,e_1]
+
+lista_nombre.sort()
+lista_RUT.sort()
+
+print(lista_nombre)
+print(lista_RUT)
